fedc0d5339907709078e9e67ee3810bcc96a5301
Kate Ne <jekanest@yahoo.com>
2024-03-28 23:51:03 +0200

    changed user and pswd

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'littlelemon',  
-        'USER': 'root',  
-        'PASSWORD': 'BasaForza_349!',  
+        'USER' : 'manager',
+        'PASSWORD' : 'manager@123!',
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
